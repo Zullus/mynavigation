40dc5d13b070375d7d9377d0fdcb87bbbda33c03
Edson Santos <edson.santos@gmail.com>
2021-05-28 16:34:01 -0300

    Navegação do Stack com parametros

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -4,12 +4,39 @@ import { View, Text } from 'react-native';
 export default function Contacts({navigation}){
 
     return(
-
-            <View style={{marginTop:60}}>
-            <Text>Nome do Contato</Text>
-            <Text
-                onPress={()=>navigation.navigate('Information')}
-            >Information...</Text>
+        <View style={{marginTop:60}}>
+            <View>
+                <Text>Nome: João Botelho</Text>
+                <Text>Telefone: (11) 93300-8587</Text>
+                <Text
+                onPress={()=> navigation.navigate('Information', 
+                {
+                nome:'João Augusto',
+                telefone:'(11)98963-5489',
+                endereco: 'Rua das araucarias',
+                numero: '1759',
+                profissao: 'Carpinteiro',
+                email:'joaobotelho@carpintariabotelho.com.br'
+                }
+                )}
+                >Detalhes...</Text>
+            </View>
+            <View style={{marginTop:20}}>
+                <Text>Nome: João Botelho</Text>
+                <Text>Telefone: (11) 93300-8587</Text>
+                <Text
+                onPress={()=> navigation.navigate('Information', 
+                {
+                nome:'Amanda Silva',
+                telefone:'(19) 96587-5222',
+                endereco: 'Av. Senna Nogueira',
+                numero: '3958',
+                profissao: 'Desenvolvedora de Softwares',
+                email:'amandasilvadeveloper@gmail.com'
+                }
+                )}
+                >Detalhes...</Text>
+            </View>
         </View>
         
     )
--- src/pages/Informations/index.js
+++ src/pages/Informations/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Information({navigation}){
+export default function Information({route, navigation}){
 
     return(
         
         <View style={{marginTop:60}}>
-            <Text>Information</Text>
-            <Text onPress={()=>navigation.navigate('Contacts')}>Voltar</Text>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>Endereço: {route.params?.endereco}</Text>
+            <Text>N°: {route.params?.numero}</Text>
+            <Text>Profissão: {route.params?.profissao}</Text>
+            <Text>Email: {route.params?.email}</Text>
+            <Text onPress={()=> navigation.navigate('Contacts')}>Voltar</Text>
         </View>
         
     )
