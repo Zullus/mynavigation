89c3b4a05beb71a8faf7720852a991b9383e6e9b
Edson Santos <edson.santos@gmail.com>
2021-05-28 17:36:09 -0300

    Navegação do Stack com Tab Navigator

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 
+import AppContacts from './src/pages/AppContacts';
 import Contacts from './src/pages/Contacts';
 import Information from './src/pages/Informations';
 
-//Com Stack Navigation
-import { createStackNavigator } from '@react-navigation/stack'
+//Com Stack Navigation e BottomTabs
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// function Tabs(){
+//   return(
+//     <Tab.Navigator>
+//       <Tab.Screen name="AppContacts" component="{AppContacts}"/>
+//       <Tab.Screen name="Contacts" component="{Contacts}"/>
+//     </Tab.Navigator>
+//   )
+// }
+
+function Tabs() {
+  return(
+    <Tab.Navigator >
+       <Tab.Screen name="Meus Contatos" component={AppContacts}/>
+       <Tab.Screen name="Contacts" component={Contacts}/>
+     </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
    <NavigationContainer>
-     <Stack.Navigator >{/* DICA */}
+     <Stack.Navigator >
        <Stack.Screen 
-        name="Contacts" 
-        component={Contacts}
+        name="Meus Contatos" 
+        component={Tabs}
 
       />
        <Stack.Screen 
